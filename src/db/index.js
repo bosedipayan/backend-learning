d74efda645eaede1169bd422fc95ea0f2acffe80
bosedipayan <bosedipayan2000@gmail.com>
2024-05-27 14:06:20 +0530

    Modified app js portion with cors & middleware

--- src/db/index.js
+++ src/db/index.js
@@ -3,13 +3,15 @@ import { DB_NAME } from '../constants.js'
 
 const connectDB = async() => {
     try{
-        const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     }
-    catch(error){
-        console.log("MONGODB Connection error", error);
-        process.exit(1)
+    catch(err)
+    {
+        console.log("MONGODB connection FAILED", err);
+        process.exit(1);
     }
 }
 
-export default connectDB;
+export default connectDB
--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 // require('dotenv').config({path: './env'})
-import dotenv from "dotenv"
+import dotenv from "dotenv"  // improve version
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`)
+
+        app.on("error", (error)=>{
+            console.log("ERROR", error);
+            throw error
+        })
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed !!!", err);
+})
 
 
 
